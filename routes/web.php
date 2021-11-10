5599018e6d40e895d64a9e4981fb3a0677bc26e9
yang yu chun <3a732024@gm.student.ncut.edu.tw>
2021-11-10 20:56:28 +0800

    Model 和 Collection 的差異

--- routes/web.php
+++ routes/web.php
@@ -52,7 +52,20 @@ Route::get('/', function () {
 
 //    Post::destroy(2);
 
-    Post::destroy(3, 5, 7);
+//    Post::destroy(3, 5, 7);
+
+//    $allPosts=Post::all();
+//    dd($allPosts);
+
+//    $featurePosts=Post::where('is_feature',1)->get();
+//    dd($featurePosts);
+
+//    $fourthPost=Post::find(4);
+//    dd($fourthPost);
+
+    $lastPost=Post::orderBy('id', 'DESC')->first();
+    dd($lastPost);
+
 });
 
 Route::get('posts', [\App\Http\Controllers\PostsController::class, 'index'])->name('posts.index');
